4af57fb8f9f8c288b2f24186201df0329b26c462
peterfalk2 <106140224+peterfalk2@users.noreply.github.com>
2022-07-28 16:24:36 +1000

    all text added

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ function show1() {
             .style.display = "none";
     document.getElementById('clouds')
             .style.display = "block";
-    document.querySelector(".words").style.fontFamily = "cursive";
+    document.querySelector(".words1").style.fontFamily = "cursive";
+    document.querySelector(".words2").style.fontFamily = "cursive";
+    document.querySelector(".words3").style.fontFamily = "cursive";
+    document.querySelector(".words4").style.fontFamily = "cursive";
+    document.querySelector(".words5").style.fontFamily = "cursive";
+    document.querySelector(".words6").style.fontFamily = "cursive";
+    document.querySelector(".words7").style.fontFamily = "cursive";
 
   }
   
@@ -20,8 +26,14 @@ function show1() {
             .style.display = "none";
     document.getElementById('leather')
             .style.display = "block";
-    document.querySelector(".words").style.fontFamily = "'Courier New', Courier, monospace";
-  }
+    document.querySelector(".words1").style.fontFamily = "'Courier New', Courier, monospace";
+    document.querySelector(".words2").style.fontFamily = "'Courier New', Courier, monospace";
+    document.querySelector(".words3").style.fontFamily = "'Courier New', Courier, monospace";
+    document.querySelector(".words4").style.fontFamily = "'Courier New', Courier, monospace";
+    document.querySelector(".words5").style.fontFamily = "'Courier New', Courier, monospace";
+    document.querySelector(".words6").style.fontFamily = "'Courier New', Courier, monospace";
+    document.querySelector(".words7").style.fontFamily = "'Courier New', Courier, monospace";
+}
   
   function show3() {
     document.getElementById('clouds1')
@@ -32,8 +44,14 @@ function show1() {
             .style.display = "none";
     document.getElementById('leaves')
             .style.display = "block";      
-    document.querySelector(".words").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
-  }  
+    document.querySelector(".words1").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+    document.querySelector(".words2").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+    document.querySelector(".words3").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+    document.querySelector(".words4").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+    document.querySelector(".words5").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+    document.querySelector(".words6").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+    document.querySelector(".words7").style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+}  
   
   
   document.getElementById(`firstName`).onkeyup = function () {
@@ -51,6 +69,18 @@ function show1() {
           document.getElementById(`firstName2Initials`).innerText = `${name1[0]} & ${name2[0]}`;
           
   }
-  
 
+  document.getElementById(`surname`).onkeyup = function () {
+        let surname1 = document.getElementById(`surname`).value;
+        document.getElementById(`surname2`).innerText = surname1;
+  }
 
+  document.getElementById(`weddingDate`).onkeyup = function () {
+        let weddingDate1 = document.getElementById(`weddingDate`).value;
+        document.getElementById(`weddingDate2`).innerText = weddingDate1;
+  }
+
+  document.getElementById(`weddingLocation`).onkeyup = function () {
+        let weddingLocation1 = document.getElementById(`weddingLocation`).value;
+        document.getElementById(`weddingLocation2`).innerText = weddingLocation1;
+  }
