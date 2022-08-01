9e1d8f5b7523a5a04d427d61ee65efc504de5923
peterfalk2 <106140224+peterfalk2@users.noreply.github.com>
2022-08-01 14:58:36 +1000

    added Ternary Expression and Media Query

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Hide all but the one background image that we want to see:
 function show1() {
     document.getElementById('roses1')
            .style.display = "none";
@@ -7,6 +9,7 @@ function show1() {
             .style.display = "none";
     document.getElementById('roses')
             .style.display = "block";
+// Is there more succinct code for what follows?:
     document.querySelector(".words1").style.fontFamily = "cursive";
     document.querySelector(".words2").style.fontFamily = "cursive";
     document.querySelector(".words3").style.fontFamily = "cursive";
@@ -57,20 +60,30 @@ function show1() {
   document.getElementById(`firstName`).onkeyup = function () {
           let name1 = document.getElementById(`firstName`).value;
           document.getElementById(`firstName2`).innerText = name1;
-          document.getElementById(`firstName2Initials`).innerText = name1[0];
+// Selecting the first initial of the name (which is position 0) and display that or nothing (an empty string):
+          document.getElementById(`firstName2Initials`).innerText = name1[0] || ``;     
   }
-  
+ 
 
   
   document.getElementById(`secondName`).onkeyup = function () {
           let name1 = document.getElementById(`firstName`).value;
           let name2 = document.getElementById(`secondName`).value;
-          document.getElementById(`firstName2`).innerText = `${name1} & ${name2}`;
-          document.getElementById(`firstName2Initials`).innerText = `${name1[0]} & ${name2[0]}`;
-          
-  }
+// name1 and name 2 are not to be empty strings:
+          const a1 = name1 !== ``;
+          const a2 = name2 !== ``;
+// Both a1 and a2 must be true (i.e. both are not to be empty strings):   
+          const A = (a1 && a2);
 
-  document.getElementById(`surname`).onkeyup = function () {
+// So, using a Ternary Expression, (condition ? return if true : return if false;)
+//if A is true, the first string is displayed and if A is false the second (empty) string is displayed:
+          document.getElementById(`firstName2`).innerText = A ? `${name1} & ${name2}` : ``;
+          document.getElementById(`firstName2Initials`).innerText = A ? `${name1[0]} & ${name2[0]}` : ``;
+} 
+   
+
+
+   document.getElementById(`surname`).onkeyup = function () {
         let surname1 = document.getElementById(`surname`).value;
         document.getElementById(`surname2`).innerText = surname1;
   }
